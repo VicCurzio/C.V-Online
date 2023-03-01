021958bfd210816daabb8bd28a932d34c92d23a5
Victor Roberto Curzio <victor.curzio@hotmail.com>
2023-03-01 18:16:16 -0300

    Proyecto finalizado

---
+++ function.js
@@ -0,0 +1,9 @@
+document.getElementById("boton__name").onclick = function () {
+    console.log("evento click")
+    document.getElementById("name").innerHTML = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat voluptate velit sapiente eveniet maxime, obcaecati vel adipisci corrupti aliquid iure quasi praesentium in corporis ? Quasi distinctio nam ut sapiente maxime."
+}
+
+document.getElementById("boton__name1").onclick = function () {
+    console.log("evento click")
+    document.getElementById("name1").innerHTML = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat voluptate velit sapiente eveniet maxime, obcaecati vel adipisci corrupti aliquid iure quasi praesentium in corporis ? Quasi distinctio nam ut sapiente maxime."
+}
